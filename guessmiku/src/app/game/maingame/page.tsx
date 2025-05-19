d60a53a7ac5394b7b0c06cbfe56f255aeacd08f1
skyfjy <skyfjy123@gmail.com>
2025-05-19 21:23:14 +0900

    refactor: remove unused imports from main game page

--- guessmiku/src/app/game/maingame/page.tsx
+++ guessmiku/src/app/game/maingame/page.tsx
@@ -1,10 +1,7 @@
-import { vocaloidSong } from "../../lib/testdata";
 import AutocompleteClient from "./AutocompleteClient";
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
